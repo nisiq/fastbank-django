b73fae93a0b3895b1cb266275d757aebf31d9277
nisiq <nicole.siq2018@gmail.com>
2023-11-23 13:58:34 -0300

    withdrawal function

--- api/views.py
+++ api/views.py
@@ -9,6 +9,8 @@ from core.models import Conta
 from api import serializers
 import random, decimal
 
+from rest_framework.decorators import action
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     "SELECT * FROM contas";
@@ -53,3 +55,34 @@ class AccountViewSet(viewsets.ModelViewSet):
             conta.save()
 
             return Response({'message':'Created'}, status=status.HTTP_201_CREATED)
+        
+    #REALIZAR SAQUE
+    @action(methods=['POST'], detail=True, url_path='sacar')
+    def sacar(self, request, pk=None):
+        conta = Conta.objects.filter(id=pk).first() # Pegar a primeira conta que encontrar
+
+        serializer_recebido = serializers.SaqueSerializer(request=request.data)
+
+        # Se for valido e existir uma conta
+        if serializer_recebido.is_valid() and conta:
+                                        #pegar o valor           #pegar o dado validado 
+            valor_saque = decimal.Decimal(serializer_recebido.validated_data.get('value')) #value em serializert
+            # Pegar o saldo da pessoa
+            saldo = decimal.Decimal(conta.saldo)
+
+            comparacao = saldo.compare(valor_saque)
+            # O valor que quero sacar tem o mesmo valor de saldo, ou menos que o saldo
+            if comparacao == 0 or comparacao == 1:
+                print(saldo - valor_saque)
+                #se o que descontar for menor ou igual a 0 = desconto - 0, 
+                # se nao = saldo - saque
+                novo_valor = 0 if saldo - valor_saque <= 0  else saldo - valor_saque
+
+                conta.saldo = novo_valor
+                conta.save()
+
+                return Response({"saldo": conta.saldo}, status=status.HTTP_200_OK)
+            
+            return Response({'message': 'Saldo Insuficiente'}, status=status.HTTP_403_FORBIDDEN)
+        return Response(serializer_recebido.errors, status=status.HTTP_400_BAD_REQUEST)
+
